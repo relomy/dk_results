8b8c98d347b6f96033ad041a0ddb2702863b9d91
Adam <relomy@gmail.com>
2019-12-06 23:29:34 -0500

    fixed more pylint issues with classes/dfssheet.py

--- classes/dfssheet.py
+++ classes/dfssheet.py
@@ -18,28 +18,32 @@ class Sheet:
         self.service = self.setup_service()
 
         # unique ID for DFS Ownership/Value spreadsheet
-        self.SPREADSHEET_ID = "1Jv5nT-yUoEarkzY5wa7RW0_y0Dqoj8_zDrjeDs-pHL4"
+        self.spreadsheet_id = "1Jv5nT-yUoEarkzY5wa7RW0_y0Dqoj8_zDrjeDs-pHL4"
 
     def setup_service(self):
-        SCOPES = "https://www.googleapis.com/auth/spreadsheets"
-        dir = "."
-        store = file.Storage(path.join(dir, "token.json"))
+        """Sets up the service for the spreadsheet."""
+        scopes = "https://www.googleapis.com/auth/spreadsheets"
+        directory = "."
+        store = file.Storage(path.join(directory, "token.json"))
         creds = store.get()
         if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets(path.join(dir, "token.json"), SCOPES)
+            flow = client.flow_from_clientsecrets(
+                path.join(directory, "token.json"), scopes
+            )
             creds = tools.run_flow(flow, store)
         return build(
             "sheets", "v4", http=creds.authorize(Http()), cache_discovery=False
         )
 
     def find_sheet_id(self, title):
+        """Find the spreadsheet ID based on title."""
         sheet_metadata = (
-            self.service.spreadsheets().get(spreadsheetId=self.SPREADSHEET_ID).execute()
+            self.service.spreadsheets().get(spreadsheetId=self.spreadsheet_id).execute()
         )
         sheets = sheet_metadata.get("sheets", "")
         for sheet in sheets:
             if title in sheet["properties"]["title"]:
-                # logger.debug("Sheet ID for {} is {}".format(title, sheet["properties"]["sheetId"]))
+                # logger.debug("Sheet ID for %s is %s", title, sheet["properties"]["sheetId"])
                 return sheet["properties"]["sheetId"]
 
     def write_values_to_sheet_range(self, values, cell_range):
@@ -50,40 +54,40 @@ class Sheet:
             self.service.spreadsheets()
             .values()
             .update(
-                spreadsheetId=self.SPREADSHEET_ID,
+                spreadsheetId=self.spreadsheet_id,
                 range=cell_range,
                 valueInputOption=value_input_option,
                 body=body,
             )
             .execute()
         )
-        logger.info("{0} cells updated.".format(result.get("updatedCells")))
+        logger.info("%s cells updated.", result.get("updatedCells"))
 
-    def get_values_from_self_range(self):
+    # def get_values_from_self_range(self):
+    #     result = (
+    #         self.service.spreadsheets()
+    #         .values()
+    #         .get(spreadsheetId=self.spreadsheet_id, range=self.cell_range)
+    #         .execute()
+    #     )
+    #     return result.get("values", [])
+
+    def get_values_from_range(self, cell_range):
         result = (
             self.service.spreadsheets()
             .values()
-            .get(spreadsheetId=self.SPREADSHEET_ID, range=self.cell_range)
+            .get(spreadsheetId=self.spreadsheet_id, range=cell_range)
             .execute()
         )
         return result.get("values", [])
 
-    def get_values_from_range(self, range):
-        result = (
-            self.service.spreadsheets()
-            .values()
-            .get(spreadsheetId=self.SPREADSHEET_ID, range=range)
-            .execute()
-        )
-        return result.get("values", [])
+    # def sheet_letter_to_index(self, letter):
+    #     """1-indexed"""
+    #     return ord(letter.lower()) - 96
 
-    def sheet_letter_to_index(self, letter):
-        """1-indexed"""
-        return ord(letter.lower()) - 96
-
-    def header_index_to_letter(self, header):
-        """1-indexed"""
-        return chr(self.columns.index(header) + 97).upper()
+    # def header_index_to_letter(self, header):
+    #     """1-indexed"""
+    #     return chr(self.columns.index(header) + 97).upper()
 
 
 class DFSSheet(Sheet):
@@ -136,7 +140,7 @@ class DFSSheet(Sheet):
         """Write a set of values to a column in a spreadsheet."""
         # set range based on column e.g. PGAMain!I2:I
         cell_range = f"{self.sport}!{column}2:{column}"
-        return super().write_values_to_sheet_range(cell_range, values)
+        self.write_values_to_sheet_range(values, cell_range)
 
     def add_last_updated(self, dt_updated):
         """Update timestamp for sheet."""
@@ -175,9 +179,9 @@ class DFSSheet(Sheet):
             if i < lineup_mod:
                 all_lineup_values.extend(values)
             elif i >= lineup_mod:
-                for j, z in enumerate(values):
+                for j, k in enumerate(values):
                     mod = (i % lineup_mod) + ((i % lineup_mod) * sport_mod) + j
-                    all_lineup_values[mod].extend([""] + z)
+                    all_lineup_values[mod].extend([""] + k)
 
             # add extra row to values for spacing if needed
             if i != lineup_mod:
@@ -186,14 +190,23 @@ class DFSSheet(Sheet):
             all_lineup_values, f"{self.sport}!{cell_range}"
         )
 
-    def get_players(self):
-        return [row[self.columns.index("Name")] for row in self.values]
+    # def get_players(self):
+    #     return [row[self.columns.index("Name")] for row in self.values]
 
 
 # class DFSsheet_TEN(DFSsheet):
 #     def __init__(self):
 #         # current PGA sheet columns
-#         columns = ["Position", "Name", "Team", "Matchup", "Salary", "Ownership", "Points", "Values"]
+#         columns = [
+#             "Position",
+#             "Name",
+#             "Team",
+#             "Matchup",
+#             "Salary",
+#             "Ownership",
+#             "Points",
+#             "Values",
+#         ]
 #         sport = "TEN"
 #         cell_range = f"A2:E"
 
