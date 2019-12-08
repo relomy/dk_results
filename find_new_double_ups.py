e70b954bb46f706955876de6a90b9b688bb73adf
Adam <relomy@gmail.com>
2019-12-08 12:07:38 -0500

    moved Contest class to its own file

--- find_new_double_ups.py
+++ find_new_double_ups.py
@@ -9,6 +9,8 @@ from bs4 import BeautifulSoup
 import browsercookie
 import requests
 
+from classes.contest import Contest
+
 COOKIES = browsercookie.chrome()
 HEADERS = {
     "Accept": "*/*",
@@ -26,42 +28,6 @@ HEADERS = {
     ),
     "X-Requested-With": "XMLHttpRequest",
 }
-
-
-class Contest:
-    """Object representing a DraftKings contest from json."""
-
-    def __init__(self, contest, sport):
-        self.sport = sport
-        self.start_date = contest["sd"]
-        self.name = contest["n"]
-        self.id = contest["id"]
-        self.draft_group = contest["dg"]
-        self.total_prizes = contest["po"]
-        self.entries = contest["m"]
-        self.entry_fee = contest["a"]
-        self.entry_count = contest["ec"]
-        self.max_entry_count = contest["mec"]
-        self.attr = contest["attr"]
-        self.is_guaranteed = False
-        self.is_double_up = False
-
-        self.start_dt = self.get_dt_from_timestamp(self.start_date)
-
-        if "IsDoubleUp" in self.attr:
-            self.is_double_up = self.attr["IsDoubleUp"]
-
-        if "IsGuaranteed" in self.attr:
-            self.is_guaranteed = self.attr["IsGuaranteed"]
-
-    @staticmethod
-    def get_dt_from_timestamp(timestamp_str):
-        """Convert timestamp to datetime object."""
-        timestamp = float(re.findall(r"[^\d]*(\d+)[^\d]*", timestamp_str)[0])
-        return datetime.datetime.fromtimestamp(timestamp / 1000)
-
-    def __str__(self):
-        return f"{vars(self)}"
 
 
 def get_dk_lobby(url):
