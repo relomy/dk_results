070a10a1016c9c665269eb1446d5593734af91d2
Adam Lewandowski <adam.p.lewandowski@nasa.gov>
2021-10-02 16:46:11 -0400

    removed some commented code and renamed file

--- update_contests.py
+++ update_contests.py
@@ -10,11 +10,11 @@ from selenium import webdriver
 
 
 # load the logging configuration
-#logging.config.fileConfig("logging.ini")
+# logging.config.fileConfig("logging.ini")
 
 logger = logging.getLogger(__name__)
 
-coloredlogs.install(level='DEBUG')
+coloredlogs.install(level="DEBUG", logger=logger)
 
 
 def check_contests_for_completion(conn):
@@ -28,10 +28,8 @@ def check_contests_for_completion(conn):
 
     logger.debug("found %i incomplete contests", len(incomplete_contests))
 
-    contests_to_update = []
     # start chromium driver
     logger.debug("starting driver")
-    # driver = start_chromedriver()
     bin_chromedriver = getenv("CHROMEDRIVER")
     if not getenv("CHROMEDRIVER"):
         raise "Could not find CHROMEDRIVER in environment"
@@ -50,44 +48,71 @@ def check_contests_for_completion(conn):
         service.service_url, desired_capabilities=options.to_capabilities()
     )
 
-    for dk_id, positions_paid, status, completed in incomplete_contests:
+    skip_draft_groups = []
+
+    for (
+        dk_id,
+        draft_group,
+        positions_paid,
+        status,
+        completed,
+        name,
+        start_date,
+    ) in incomplete_contests:
+        if draft_group in skip_draft_groups:
+            logger.debug(
+                "skipping %s because we've already updated %d [skipped draft groups %s]",
+                name,
+                draft_group,
+                " ".join(str(dg) for dg in skip_draft_groups),
+            )
+            continue
+
         # navigate to the gamecenter URL
         url = f"https://www.draftkings.com/contest/gamecenter/{dk_id}"
         logger.debug("driver.get url %s", url)
         driver.get(url)
 
-        logger.debug("getting contest data for %i", dk_id)
+        logger.debug(
+            "getting contest data for %s [id: %i start: %s dg: %d]",
+            name,
+            dk_id,
+            start_date,
+            draft_group,
+        )
         contest_data = get_contest_data(driver.page_source, dk_id)
 
         if not contest_data:
             continue
 
-        # if contest data is different, append list
+        # if contest data is different, update it
         if (
             positions_paid != contest_data["positions_paid"]
             or status != contest_data["status"]
             or completed != contest_data["completed"]
         ):
-            logger.debug("add contest %i to contests_to_update", dk_id)
-            contests_to_update.append(
-                (
+            #            logger.debug("trying to update contest %i", dk_id)
+            db_update_contest(
+                conn,
+                [
                     contest_data["positions_paid"],
                     contest_data["status"],
                     contest_data["completed"],
                     dk_id,
-                )
+                ],
             )
+        else:
+            # if contest data is the same, don't update other contests in the same draft group
+            skip_draft_groups.append(draft_group)
+            logger.debug("contest data is the same, not updating")
 
     logger.debug("quitting driver")
     driver.quit()
 
-    if contests_to_update:
-        # db_check_contests_for_update(conn, contests_to_update)
-        db_update_contest_data_for_contests(conn, contests_to_update)
 
-
-def db_update_contest_data_for_contests(conn, contests_to_update):
+def db_update_contest(conn, contest_to_update):
     """Update contest fields based on get_contest_data()."""
+    logger.debug("trying to update contest %i", contest_to_update[3])
     cur = conn.cursor()
 
     sql = (
@@ -97,7 +122,7 @@ def db_update_contest_data_for_contests(conn, contests_to_update):
     )
 
     try:
-        cur.executemany(sql, contests_to_update)
+        cur.execute(sql, contest_to_update)
         conn.commit()
         logger.info("Total %d records updated successfully!", cur.rowcount)
     except sqlite3.Error as err:
@@ -112,7 +137,7 @@ def db_get_incomplete_contests(conn):
     try:
         # execute SQL command
         sql = (
-            "SELECT dk_id, positions_paid, status, completed "
+            "SELECT dk_id, draft_group, positions_paid, status, completed, name, start_date "
             "FROM contests "
             "WHERE start_date <= datetime('now', 'localtime') "
             "  AND (positions_paid IS NULL OR completed = 0)"
@@ -141,14 +166,16 @@ def get_contest_data(html, contest_id):
     soup = BeautifulSoup(html, "html.parser")
 
     try:
+        logger.debug("looking for entries...")
         entries = soup.find("label", text="Entries").find_next("span").text
+        logger.debug("entries: %s", entries)
+
         status = soup.find("label", text="Status").find_next("span").text.upper()
+        logger.debug("status: %s", status)
+
         positions_paid = (
             soup.find("label", text="Positions Paid").find_next("span").text
         )
-
-        logger.debug("entries: %s", entries)
-        logger.debug("status: %s", status)
         logger.debug("positions_paid: %s", positions_paid)
 
         if status in ["COMPLETED", "LIVE", "CANCELLED"]:
@@ -169,9 +196,8 @@ def get_contest_data(html, contest_id):
         # This error occurs for old contests whose pages no longer are being served.
         # IndexError: list index out of range
         # logger.debug("driver.get url %s", driver.current_url)
-        logger.error(
-            "Couldn't find DK contest with id %d error: %s", contest_id, ex,
-        )
+        logger.error("Couldn't find DK contest with id %d error: %s", contest_id, ex)
+        print(html, file=open("debug.html", "w"))
 
 
 def main():
