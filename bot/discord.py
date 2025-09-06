cbcd8dd819933fd097716562761ff0361679f41e
Adam Lewandowski <adam.p.lewandowski@nasa.gov>
2025-09-06 13:45:58 -0400

    remove unused discordbk.py file

--- bot/discord.py
+++ bot/discord.py
@@ -1,6 +1,5 @@
 import requests
-import json
-import logging
+
 from .botinterface import BotInterface
 
 
@@ -8,8 +7,6 @@ class Discord(BotInterface):
     def __init__(self, webhook):
         self.webhook = webhook
 
-
     def send_message(self, message):
         payload = {"content": message}
         requests.post(self.webhook, json=payload)
-
