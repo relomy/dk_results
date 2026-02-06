a22afecd078e7b814b397e897c09a919ca331dc1
Adam Lewandowski <adam.p.lewandowski@nasa.gov>
2026-02-06 15:15:09 -0500

    test(find_new_double_ups): cover 69-70,90-95,110-118,148-162,176-366,384,420-424,437-463,529,551-557,564,594-604,623,645,662-684,688

--- tests/test_find_new_double_ups.py
+++ tests/test_find_new_double_ups.py
@@ -1,7 +1,27 @@
+import argparse
 import datetime
+import types
 
-from classes.sport import NFLSport
-from find_new_double_ups import build_draft_group_start_map, process_sport
+import pytest
+
+from classes.contest import Contest
+from classes.sport import NFLShowdownSport, NFLSport, Sport
+from find_new_double_ups import (
+    build_draft_group_start_map,
+    contest_meets_criteria,
+    format_discord_messages,
+    get_contests_from_response,
+    get_double_ups,
+    get_draft_groups_from_response,
+    get_salary_date,
+    get_stats,
+    is_time_between,
+    log_draft_group_event,
+    process_sport,
+    send_discord_notification,
+    set_quiet_verbosity,
+    valid_date,
+)
 
 
 def test_build_draft_group_start_map_filters_and_parses():
@@ -78,3 +98,293 @@ def test_process_sport_syncs_draft_group_start_dates(monkeypatch):
         111: datetime.datetime(2024, 2, 1, 12, 30),
         222: datetime.datetime(2024, 2, 2, 13, 0),
     }
+
+
+def _contest_payload(dk_id: int):
+    return {
+        "sd": "1700000000000",
+        "n": "Contest",
+        "id": dk_id,
+        "dg": 10,
+        "po": 0,
+        "m": 200,
+        "a": 10,
+        "ec": 0,
+        "mec": 1,
+        "attr": {"IsDoubleUp": True, "IsGuaranteed": True},
+        "gameType": "Classic",
+        "gameTypeId": 1,
+    }
+
+
+def test_send_discord_notification_no_bot():
+    send_discord_notification(None, "NBA", "msg")
+
+
+def test_send_discord_notification_formats_message():
+    class FakeBot:
+        def __init__(self):
+            self.sent = []
+
+        def send_message(self, message: str):
+            self.sent.append(message)
+
+    bot = FakeBot()
+    send_discord_notification(bot, "NBA", "New contest")
+    assert bot.sent == [":basketball: New contest <@&1034206287153594470>"]
+
+
+def test_get_contests_from_response_list_and_dict():
+    assert get_contests_from_response([{1: 2}]) == [{1: 2}]
+    assert get_contests_from_response({"Contests": [1]}) == [1]
+
+
+def test_get_contests_from_response_invalid(monkeypatch):
+    monkeypatch.setattr(
+        "find_new_double_ups.sys.exit",
+        lambda *_args, **_kwargs: (_ for _ in ()).throw(SystemExit()),
+    )
+    with pytest.raises(SystemExit):
+        get_contests_from_response({"Other": []})
+
+
+def test_log_draft_group_event_includes_reason(monkeypatch):
+    captured = []
+
+    def fake_log(level, msg, *args):
+        captured.append(msg % args)
+
+    monkeypatch.setattr("find_new_double_ups.logger.log", fake_log)
+
+    class DummySport(Sport):
+        name = "TEST"
+
+    log_draft_group_event(
+        "Skip",
+        DummySport,
+        datetime.datetime(2024, 1, 1, 0, 0, 0),
+        10,
+        "Featured",
+        "(Main)",
+        1,
+        2,
+        reason="suffix mismatch",
+    )
+
+    assert any("reason" in msg for msg in captured)
+
+
+def test_get_draft_groups_from_response_filters():
+    class DummySport(Sport):
+        name = "TEST"
+        suffixes = [r"\(Main\)"]
+        allow_suffixless_draft_groups = False
+        contest_restraint_time = datetime.time(18, 0)
+        contest_restraint_game_type_id = 99
+
+    response = {
+        "DraftGroups": [
+            {
+                "DraftGroupTag": "Other",
+                "ContestStartTimeSuffix": "(Main)",
+                "DraftGroupId": 1,
+                "StartDateEst": "2024-02-01T12:30:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 99,
+            },
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": None,
+                "DraftGroupId": 2,
+                "StartDateEst": "2024-02-01T12:30:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 99,
+            },
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": "(Alt)",
+                "DraftGroupId": 3,
+                "StartDateEst": "2024-02-01T12:30:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 99,
+            },
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": "(Main)",
+                "DraftGroupId": 4,
+                "StartDateEst": "2024-02-01T12:30:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 1,
+            },
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": "(Main)",
+                "DraftGroupId": 5,
+                "StartDateEst": "2024-02-01T16:00:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 99,
+            },
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": "(Main)",
+                "DraftGroupId": 6,
+                "StartDateEst": "2024-02-01T20:00:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 99,
+            },
+        ]
+    }
+
+    result = get_draft_groups_from_response(response, DummySport)
+    assert result == [6]
+
+
+def test_get_draft_groups_from_response_nfl_showdown():
+    response = {
+        "DraftGroups": [
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": "(ABC @ DEF)",
+                "DraftGroupId": 10,
+                "StartDateEst": "2024-02-01T20:00:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 96,
+            },
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": "(ABC @ DEF)",
+                "DraftGroupId": 11,
+                "StartDateEst": "2024-02-01T20:00:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 96,
+            },
+            {
+                "DraftGroupTag": "Featured",
+                "ContestStartTimeSuffix": "(XYZ @ QRS)",
+                "DraftGroupId": 12,
+                "StartDateEst": "2024-02-01T21:00:00.000-05:00",
+                "ContestTypeId": 1,
+                "GameTypeId": 96,
+            },
+        ]
+    }
+
+    result = get_draft_groups_from_response(response, NFLShowdownSport)
+    assert result == [12]
+
+
+def test_build_draft_group_start_map_empty():
+    assert build_draft_group_start_map([], set()) == {}
+
+
+def test_valid_date_rejects_invalid():
+    with pytest.raises(argparse.ArgumentTypeError):
+        valid_date("bad")
+
+
+def test_get_stats_counts_dubs():
+    contests = [Contest(_contest_payload(1), "NBA")]
+    stats = get_stats(contests)
+    assert stats[contests[0].start_dt.strftime("%Y-%m-%d")]["dubs"][10] == 1
+
+
+def test_get_double_ups_filters():
+    contests = [Contest(_contest_payload(1), "NBA")]
+    result = get_double_ups(contests, [10])
+    assert [c.id for c in result] == [1]
+
+
+def test_contest_meets_criteria_false():
+    contest = Contest(_contest_payload(1), "NBA")
+    assert (
+        contest_meets_criteria(
+            contest,
+            {
+                "entries": 300,
+                "draft_groups": [],
+                "min_entry_fee": 5,
+                "max_entry_fee": 50,
+            },
+        )
+        is False
+    )
+
+
+def test_get_salary_date():
+    date_val = get_salary_date({"StartDateEst": "2024-02-01T12:30:00.000-05:00"})
+    assert date_val == datetime.date(2024, 2, 1)
+
+
+def test_is_time_between_crosses_midnight():
+    begin = datetime.time(23, 0)
+    end = datetime.time(1, 0)
+    assert is_time_between(begin, end, datetime.time(0, 30)) is True
+
+
+def test_set_quiet_verbosity():
+    set_quiet_verbosity()
+
+
+def test_format_discord_messages():
+    contests = [Contest(_contest_payload(1), "NBA")]
+    msg = format_discord_messages(contests)
+    assert "New dub found!" in msg
+
+
+def test_process_sport_invalid_name(monkeypatch):
+    with pytest.raises(Exception):
+        process_sport("BAD", {}, None, None)
+
+
+def test_process_sport_sends_notification(monkeypatch):
+    contest = _contest_payload(1)
+    draft_groups = [10]
+
+    def fake_get_dk_lobby(_sport, _url):
+        response = {
+            "DraftGroups": [
+                {
+                    "DraftGroupId": 10,
+                    "StartDateEst": "2024-02-01T12:30:00.000-05:00",
+                }
+            ]
+        }
+        return [contest], draft_groups, response
+
+    class FakeDB:
+        def __init__(self):
+            self.inserted = []
+
+        def create_table(self):
+            return None
+
+        def sync_draft_group_start_dates(self, _start_map):
+            return 0
+
+        def compare_contests(self, _contests):
+            return [1]
+
+        def insert_contests(self, contests):
+            self.inserted.extend(contests)
+
+    class FakeBot:
+        def __init__(self):
+            self.sent = []
+
+        def send_message(self, message: str):
+            self.sent.append(message)
+
+    monkeypatch.setattr("find_new_double_ups.get_dk_lobby", fake_get_dk_lobby)
+
+    class DummySport(Sport):
+        name = "NBA"
+        dub_min_entry_fee = 5
+        dub_min_entries = 125
+
+    db = FakeDB()
+    bot = FakeBot()
+
+    process_sport("NBA", {"NBA": DummySport}, db, bot)
+
+    assert db.inserted
+    assert bot.sent
